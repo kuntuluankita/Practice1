9f9f3583ec1f6e8c6156b97bac58af218f14038b
Kuntulu Ankita <kuntuluankita@gmail.com>
2024-07-16 14:41:43 +0530

    Initial Commit

---
+++ Practice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice1
+//
+//  Created by Kuntulu Ankita on 16/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
